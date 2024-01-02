77c9ff7ae52f7ef15ffcbe096416f2b40a8e2917
Kevin Matz <kevin@winchelseasystems.com>
2024-01-02 00:45:44 -0800

    Search Bar: Added radio buttons for sorting options; starting to add state (in progress)

--- ravenous/src/SearchBar.js
+++ ravenous/src/SearchBar.js
@@ -1,8 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function SearchBar() {
+  const [searchTerms, setSearchTerms] = useState("");
+  const [location, setLocation] = useState("");
+  const [sortingOption, setSortingOption] = useState("best-match");
+
+  function sortingOptionSelectionHandler(event) {
+    setSortingOption(event.target.value);
+  }
+
   return (
     <div className="SearchBar">
+      <input type="radio" id="best-match" name="sorting-options" value="best-match" onClick={sortingOptionSelectionHandler} />
+      <label for="best-match">&nbsp;Best Match</label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      <input type="radio" id="highest-rated" name="sorting-options" value="highest-rated" onClick={sortingOptionSelectionHandler} />
+      <label for="highest-rated">&nbsp;Highest Rated</label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      <input type="radio" id="most-reviewed" name="sorting-options" value="most-reviewed" onClick={sortingOptionSelectionHandler} />
+      <label for="most-reviewed">&nbsp;Most Reviewed</label>
+      <br/>
       <label for="searchTerms">Search for:&nbsp;</label>
       <input name="searchTerms" />
       &nbsp;&nbsp;&nbsp;&nbsp;
